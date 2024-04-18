0bfe5c748bb912accca3622b2a9557a075a9b569
mdelgado96 <d.marvin96@gmail.com>
2024-04-17 19:46:50 -0700

    Settings API Fetch

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
+
 const api_url =
-  "https://api.weatherapi.com/v1/current.json?key=96da7893841b4d90a9651147241903&q=London&aqi=no";
+  "https://api.weatherapi.com/v1/search.json?key=96da7893841b4d90a9651147241903&q=Paris";
 const searchInput = document.querySelector("[data-search]");
 const forecastCardTemplate = document.querySelector("[data-forecast-template]");
+const forecastCardContainer = document.querySelector("[data-forecast-cards-container]");
 
-searchInput.addEventListener("input", e => {
+searchInput.addEventListener("input", (e) => {
   const value = e.target.value;
   console.log(value);
 });
 
-fetch(api_url).then(res => res.json()).then(data => {
-  const forecast = forecastCardTemplate.content.cloneNode(true)
-  console.log(forecast)
-})
+fetch(api_url)
+  .then((res) => res.json())
+  .then(data => {
+    data.forEach((forecast) => {
+      const card = forecastCardTemplate.content.cloneNode(true).children[0];
+      const time = card.querySelector("[data-forecast-time]");
+      const icon = card.querySelector("[data-forecast-icon]");
+      const degrees = card.querySelector("[data-forecast-degrees]");
+      time.textContent = forecast.time;
+      icon.textContent = forecast.id;
+      degrees.textContent = forecast.lat;
+      forecastCardContainer.append(card);
+    });
+  });
 
 //   async function main() {
 //   try {
